f2e75a2b80f0516b20ee15317d284bf5a38bbaf3
Shakeel Mohamed <mmmshakeel@gmail.com>
2024-07-21 11:19:31 +0200

    Update tshark script to read the capture file from env variable

--- scripts/run_tshark.sh
+++ scripts/run_tshark.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Get the pcap file path from the environment variable
+PCAP_FILE=${PCAP_FILE:-/data/capture-1-0.pcap}
+
 # Run tshark to read the capture file incrementally and output to CSV
-tshark -r /data/capture-1-0.pcap -T fields -E separator=, -E quote=d -E occurrence=f \
+tshark -r "$PCAP_FILE" -T fields -E separator=, -E quote=d -E occurrence=f \
 -e frame.time_relative -e frame.len -e ip.src -e ip.dst -e udp.srcport -e udp.dstport -e udp.length \
 -e tcp.srcport -e tcp.dstport -e tcp.flags -e icmp.type -e icmp.code \
 -e eth.src -e eth.dst \
